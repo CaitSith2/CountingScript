82c1f675af96a8cd933e54a322c6f0688fc8daff
CaitSith2 <CaitSith2@caitsith2.com>
2020-12-06 23:03:50 -0800

    Optimize IsPrime
    Optimize IsPrime to do a check for even, then check by dividing by every odd number starting from 3.

--- src/utils.ts
+++ src/utils.ts
@@ -53,7 +53,13 @@ export function IsPrime(num: number): boolean {
     
     num = Math.round(num);
     
-    for (let i = 2; i < Math.sqrt(num); i++)
+    if (num === 2)
+        return true;
+    
+    if (num % 2 === 0)
+        return false;
+    
+    for (let i = 3; i < Math.sqrt(num); i+=2)
         if (num % i === 0)
             return false;
     
